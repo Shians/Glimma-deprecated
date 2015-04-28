b277831c7226f581a70faf9af07dc97a4bfa54c2
Shians <registertonysu@gmail.com>
2015-04-28 16:17:39 +1000

    Added clicking function to table rows
    - Table rows can now be clicked to highlight genes in MA plot.
    - Added missing icons for table.
    - Removed columns for color and (x,y) coordinates in table.
    - Table now only appears after rendering completed.

--- inst/report_page/gene_table.js
+++ inst/report_page/gene_table.js
@@ -8,6 +8,8 @@ function makeTable(targetDiv, data) {
 
 	// Set column names
 	var colnames = Object.keys(data[0]);
+	// Filter out colour and coordinate information
+	colnames = colnames.filter(function(d) {return d!="col" && d!="x" && d!="y";});
 	var headRowSelect = headSelect.append("tr");
 	colnames.forEach(function (d) {
 		headRowSelect.append("td")
@@ -23,10 +25,21 @@ function makeTable(targetDiv, data) {
 		})
 	});
 
+	// Get column number of pval
+	pValInd = colnames.indexOf("pval");
+
 	// Apply DataTable formatting: https://www.datatables.net/
 	$(document).ready(function() {
         $('#gene_table').DataTable( {
-        	"bLengthChange": false
+        	"bLengthChange": false,
+        	"order": [[ pValInd, "asc" ]]
         } );
+        $('#gene_table_wrapper').css("visibility", "visible");
     });
+
+    d3.select("#gene_table").selectAll("tr")
+    	.on("click", function(d) {
+    		var symb = d3.select(this).select("td:nth-child(5)").text();
+    		findGeneAndHighlight(symb);
+    		});
 }
