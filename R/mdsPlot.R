6dfa0be42ba9e0f5071027f55064265718ef223c
Shians <registertonysu@gmail.com>
2015-07-16 17:26:18 +1000

    MDS plot first draft
    - Basic functioning MDS plot
    - R functions to generate plot
    - CSS styles for plot
    - HTML page to contain plot

---
+++ R/mdsPlot.R
@@ -0,0 +1,111 @@
+interactiveMDSPlot <- function(x, ...) {
+  UseMethod("interactiveMDSPlot")
+}
+
+interactiveMDSPlot.default <- function(x, col, top=500, labels = NULL, gene.selection = "pairwise", dir=NULL, launch=TRUE, main=NULL) {
+	#------------------------------------------------------------
+  	# Check for errors
+  	#------------------------------------------------------------
+
+	x <- as.matrix(x)
+	nsamples <- ncol(x)
+
+	if (nsamples < 3) {
+		stop(paste("Only", nsamples, "columns of data: need at least 3"))
+	}
+
+	cn <- colnames(x)
+	bad <- rowSums(is.finite(x)) < nsamples
+
+	if (any(bad)) {
+		x <- x[!bad, drop=FALSE]
+	}
+
+	nprobes <- nrow(x)
+	top <- min(top, nprobes)
+
+	if (is.null(labels)) {
+		labels <- paste("Sample", 1:nsamples)
+	} else {
+		labels <- as.character(labels)
+	}
+
+	#------------------------------------------------------------
+	# Process input
+	#------------------------------------------------------------
+	# If no output directory is provided, set to current working directory
+	if (is.null(dir)) {
+		dir <- getwd()
+	}
+
+	# Locate files in package library
+	page.path <- system.file("report_page", package="Glimma")
+	files <- paste(page.path, c("mds_plot.js", "mds_plot.html", "plot_styles.css", "mds_styles.css", "utilities.js", "js", "css"), sep="/")
+
+	# Make report page directory and copy over data
+	if (substr(dir, nchar(dir), nchar(dir)) == "/") {
+		dir.create(paste(dir, "report_page", sep=""), showWarnings=FALSE)
+		report.path <- paste(dir, "report_page", sep="")
+	} else {
+		dir.create(paste(dir, "report_page", sep="/"), showWarnings=FALSE)
+		report.path <- paste(dir, "report_page", sep="/")
+	}
+	file.copy(from=files, to=report.path, recursive=TRUE)
+
+	data.filename <- paste(report.path, "/mds_data.js", sep="")
+
+	gene.selection <- match.arg(gene.selection, c("pairwise", "common"))
+	dd <- matrix(0, nrow=nsamples, ncol=nsamples, dimnames=list(cn,cn))
+	if (gene.selection == "pairwise") {
+		topindex <- nprobes - top + 1L
+		for (i in 2L:(nsamples)) {
+			for (j in 1L:(i - 1L)) {
+				dd[i, j] = sqrt(mean(sort.int((x[, i] - x[, j])^2, partial = topindex)[topindex:nprobes]))
+			}
+		}
+	} else if (gene.selection == "common") {
+		if (nprobes > top) {
+			s <- rowMeans((x -  rowMeans(x))^2)
+			o <- order(s, decreasing=TRUE)
+			x <- x[o[1L:top], , drop=FALSE]
+		}
+		for (i in 2L:nsamples) {
+			dd[i, 1L:(i - 1L)] = sqrt(colMeans((x[, i] - x[, 1:(i - 1), drop = FALSE])^2))
+		}
+	}
+	a1 <- suppressWarnings(cmdscale(as.dist(dd), k=min(10, nprobes), eig=TRUE))
+
+	dmatrix <- a1$points
+	eigvals <- round(a1$eig, 3)[1:10]
+	eigsum <- round(sum(a1$eig), 3)
+	
+	if (is.character(col)) {
+		cols <- apply(as.character(as.hexmode(col2rgb(col, alpha=FALSE))), 2, function(x) {paste0("#", paste0(x, collapse=""))})
+	} else if (is.factor(col) || is.numeric(col)) {
+		col <- palette()[as.integer(col)]
+		cols <- apply(as.character(as.hexmode(col2rgb(col, alpha=FALSE))), 2, function(x) {paste0("#", paste0(x, collapse=""))})
+	} else {
+		stop("input col must not of class 'interger', 'factor' or 'character'")
+	}
+	
+	#------------------------------------------------------------
+	# Generate javascript strings
+	#------------------------------------------------------------
+	
+	col <- cols
+	df <- data.frame(labels, dmatrix, col)
+	colnames(df)[1] <- "label"
+	colnames(df)[2:(ncol(df)-1)] <- 2:(ncol(df)-1) - 1
+	js <- makeDFJson(df)
+
+	eigjs <- arrayify(paste(eigvals, collapse=","))
+
+	#------------------------------------------------------------
+  	# Output
+  	#------------------------------------------------------------
+  
+	data.filename <- paste(report.path, "/mds_data.js", sep="")
+	printJsonToFile(c(js, eigjs, eigsum), filename=data.filename,
+					varname=c("dataMDS", "eigenVals", "eigenSum"))
+
+}
--- inst/report_page/mds_plot.js
+++ inst/report_page/mds_plot.js
@@ -1,21 +1,81 @@
-function mdsPlot(targetDiv, data) {
+// Function to generate axis lines
+function drawAxis(targetSvg, xScl, yScl, svgh, svgw, transition) {
+	transition = transition || false;
+
+	var xAxis = d3.svg.axis().scale(xScl).orient('bottom');
+	var top = pxToNum(svgh) - G_MARG.bottom;
+	var yAxis = d3.svg.axis().scale(yScl).orient('left');
+
+	if (targetSvg.select('.axis').node() == null) {
+		var xg = targetSvg.append('g')
+							.attr('class', 'axis x')
+							.attr('transform', trans(G_MARG.left, top));
+
+		var yg = targetSvg.append('g')
+			  				.attr('class', 'axis y')
+			  				.attr('transform', trans(G_MARG.left, G_MARG.top));
+	} else {
+		xg = targetSvg.select('.x');
+		yg = targetSvg.select('.y');
+	}
+
+	xg.call(xAxis);
+	if (transition) {
+		// d3.select('.plot.dot')
+		// 	.select('.axis.y')
+		// 	.selectAll('text')
+		// 	.transition().style('opacity', 0);
+		// setTimeout(function() { yg.call(yAxis); }, 200);
+		// d3.select('.plot.dot')
+		// 	.select('.axis.y')
+		// 	.selectAll('text')
+		// 	.transition().delay(200).style('opacity', 1);
+		yg.transition().call(yAxis);
+	} else {
+		yg.call(yAxis);
+	}
+	
+}
+
+// Function to perform actions when mouse over MDS plot
+function MDSMOver(node) {
+	var d = node.data()[0];
+
+	var x = node.attr('cx');
+	var y = node.attr('cy');
+
+	console.log(x);
+	console.log(y);
+}
+
+// Function to draw MDS plot
+function mdsPlot(targetDiv, data, dim1, dim2, colOpt) {
+
+	if (dim1 > dim2 || dim1 < 1 || dim2 > 10) {
+		return;
+	}
+
+	// Set default colour option
+	colOpt = colOpt || 'col';
+
 	// Get dimension data
 	var dim = divDim(targetDiv);
 
 	// Find limits of dataset
-	var limX = findLim(data, 'stat');
-	var limY = {'min': 0, 'max': 1};
+	var limX = findLim(data, dim1);
+	var limY = findLim(data, dim2);
 
 	var adj = (limX.max/2 - limX.min/2) * 0.05;
+	limX.max += adj;
+	limX.min -= adj
 
-	// Get quantiles for data, create dummy scale for labeling
-	var quantiles = getStatQuantiles(data);
-	var labelScl = d3.scale.ordinal().domain(quantiles)
-										.rangePoints([0, dim.pltw]);
+	adj = (limY.max/2 - limY.min/2) * 0.05;
+	limY.max += adj;
+	limY.min -= adj;
 
 	// Generate scalers
 	var xScl = d3.scale.linear().domain([limX.min, limX.max])
-								.rangePoints([0, dim.pltw]);
+								.range([0, dim.pltw]);
 	var yScl = d3.scale.linear().domain([limY.min, limY.max])
 								.range([0, dim.plth]);
 
@@ -26,7 +86,7 @@ function mdsPlot(targetDiv, data) {
 
 		var plotWindow = svgMDS.append('g')
 								.classed('plot_window', true)
-								.attr('transform', trans(G_MARG.top, G_MARG.left));
+								.attr('transform', trans(G_MARG.left, G_MARG.top));
 		var mdsContain = plotWindow.append('g')
 									.classed('mds_container', true);
 
@@ -36,6 +96,83 @@ function mdsPlot(targetDiv, data) {
 		var plotWindow = svgMDS.select('.plot_window');
 		var mdsContain = plotWindow.select('.mds_container');
 	}
-
 	
+	// Attach points
+	var points = mdsContain.selectAll('.point')
+							.data(data);
+
+	points.enter().append('circle');
+	points.attr('class', 'point')
+			.attr('r', 6)
+			.style('fill', function (d) { return d[colOpt]; })
+			.on('mouseover', function (d) { return MDSMOver(d3.select(this)); });
+
+	points.transition()
+			.attr('cx', function (d) { return xScl(d[dim1]); })
+			.attr('cy', function (d) { return yScl(d[dim2]); });
+			
+
+	drawAxis(svgMDS, xScl, yScl, dim.svgh, dim.svgw);
+	xLabel(svgMDS, 'Dimension ' + String(dim1));
+	yLabel(svgMDS, 'Dimension ' + String(dim2));
+}
+
+function skreeClick(i) {
+	var mdsDiv = d3.select('.plot_div.left');
+	var colOpt = 'col';
+	mdsPlot(mdsDiv, dataMDS, i, i+1);
+}
+
+// Function to draw skree plot
+function skreePlot(targetDiv, data) {
+	var marg = {'top': 20, 'bottom': 50, 'left': 60, 'right': 10};
+
+	// Get dimension data
+	var dim = divDim(targetDiv, marg);
+
+	// Find limits of dataset
+	var limY = d3.extent(data);
+
+	// Generate scalers
+	var xScl = d3.scale.ordinal().domain(d3.range(1,11))
+    								.rangeRoundBands([0, dim.pltw], .1);
+	var yScl = d3.scale.linear().domain([limY[1], limY[0]])
+								.range([0, dim.plth]);
+
+	// Generate window and containers for plot
+	var svgSkree = targetDiv.append('svg')
+								.classed('plot skree', true);
+
+	var plotWindow = svgSkree.append('g')
+								.classed('plot_window', true)
+								.attr('transform', trans(marg.left, marg.top));
+
+	var barContainer = plotWindow.append('g')
+									.classed('bar_container', true);
+
+	// Attach data
+	var bars = barContainer.selectAll('.bar')
+							.data(data);
+
+	// Draw bars
+	bars.enter()
+		.append('rect')
+		.attr('class', 'bar')
+		.attr('x', function (d, i) { return xScl(i + 1); })
+		.attr('y', function (d) { return yScl(d); })
+		.attr('height', function (d) { return dim.plth - yScl(d); })
+		.attr("width", xScl.rangeBand())
+		.on('click', function(d, i) { skreeClick(i+1) ;});
+
+
+	xAxis(svgSkree, xScl, dim.svgh, dim.svgw, marg);
+	yAxis(svgSkree, yScl, dim.svgh, dim.svgw, marg);
+	xLabel(svgSkree, 'Dimension');
+	yLabel(svgSkree, 'Magnitude');
+}
+
+function updateCircleRad(size) {
+	d3.select('.mds_container')
+		.selectAll('.point')
+		.attr('r', size/10);
 }
